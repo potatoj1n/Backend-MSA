dd4e73ef4b69ef81cf5a8c2c59801351aa00bb57
potatoj1n <155697505+potatoj1n@users.noreply.github.com>
2024-09-16 20:32:06 +0900

    Update PlaceController.java(젠킨스 파이프라인 테스트 1)

--- place/src/main/java/project/place/controller/PlaceController.java
+++ place/src/main/java/project/place/controller/PlaceController.java
@@ -9,7 +9,7 @@ public class PlaceController {
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("Hello Place");
+        return ResponseEntity.ok("Hello Place Test1");
     }
 
 }
