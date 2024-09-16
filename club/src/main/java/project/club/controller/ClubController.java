3c402a9befbb40b131d6ec389230fe36b55449ce
potatoj1n <155697505+potatoj1n@users.noreply.github.com>
2024-09-16 21:37:49 +0900

    Update ClubController.java (젠킨스 파이프라인 MSA 테스트7)

--- club/src/main/java/project/club/controller/ClubController.java
+++ club/src/main/java/project/club/controller/ClubController.java
@@ -9,7 +9,7 @@ public class ClubController {
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("Hello Club Test6");
+        return ResponseEntity.ok("Hello Club Test7");
     }
 
 }
