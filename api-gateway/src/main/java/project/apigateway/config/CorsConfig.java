4fddf391b8705bd20504a2c52a1181d361e50d2f
potatoj1n <155697505+potatoj1n@users.noreply.github.com>
2024-09-21 14:30:06 +0900

    Update CorsConfig.java

--- api-gateway/src/main/java/project/apigateway/config/CorsConfig.java
+++ api-gateway/src/main/java/project/apigateway/config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","http://localhost:5000"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
